22b236a09e384e5583339f8f8cd927ff6d8b1eb4
Shanza Gul <gulshanza77@gmail.com>
2023-10-12 20:46:52 +0500

    Add boilerplate

---
+++ src/app/dictionary/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const DictionaryOfDreams: React.FC = () => {
+  return <div>DOD</div>;
+};
---
+++ src/app/videos/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Videos: React.FC = () => {
+  return <div>videos</div>;
+};
