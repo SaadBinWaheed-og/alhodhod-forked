741131ce95a0a3d938550093fe860dad774d82ca
Shanza Gul <gulshanza77@gmail.com>
2023-10-25 01:52:39 +0500

    Refactor Header

--- src/app/common/Header/Header.styled.tsx
+++ src/app/common/Header/Header.styled.tsx
@@ -1,19 +1,33 @@
-import { Button } from "antd";
 import { styled } from "styled-components";
+import { Button } from "antd";
 
 export const HeaderContainer = styled.div`
   position: relative;
   width: auto;
   height: 100vh;
-  margin: 20px;
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-top: 20px;
 `;
 
-export const NavbarContainer = styled.div`
-  position: absolute;
-  top: 68px;
-  right: 98px;
-  z-index: 1;
-  padding-top: 40px;
+export const HeroWrapper = styled.div<{
+  $isHomePage: boolean;
+}>`
+  position: relative;
+  width: calc(100vw - 40px);
+  height: ${({ $isHomePage }) => ($isHomePage ? "95vh" : "75")};
+`;
+
+export const ImageWrapper = styled.div`
+  z-index: -1;
+`;
+
+export const HeroContent = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
 `;
 
 export const LogoContainer = styled.div`
@@ -21,29 +35,6 @@ export const LogoContainer = styled.div`
   top: 40px;
   left: 98px;
   z-index: 1;
-`;
-
-export const HeaderTextContainer = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  z-index: 1;
-  text-align: center;
-  width: 100%;
-`;
-
-export const HeaderText = styled.div`
-  color: white;
-  font-size: 34px;
-  font-weight: 400;
-  /* color: #c1ff0b; */
-`;
-
-export const HeaderSubText = styled.div`
-  color: white;
-  font-size: 79px;
-  font-weight: 700;
 `;
 
 export const WebsiteAddress = styled.div`
@@ -60,15 +51,57 @@ export const WebsiteSince = styled.div`
   text-align: center;
 `;
 
+export const HeaderTextContainer = styled.div<{
+  $isHomePage?: boolean;
+}>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding-top: ${({ $isHomePage }) => ($isHomePage ? "calc(30vh)" : "199px")};
+  padding-bottom: ${({ $isHomePage }) => ($isHomePage ? "0px" : "163px")};
+  text-align: center;
+`;
+
+export const HeaderText = styled.div`
+  color: white;
+  font-size: 34px;
+  font-weight: 400;
+`;
+
+export const HeaderDescription = styled.div`
+  color: white;
+  font-size: 34px;
+  font-weight: 400;
+  line-height: normal;
+  width: 1053px;
+`;
+
+export const HeaderSubText = styled.div`
+  color: white;
+  font-size: 79px;
+  font-weight: 700;
+`;
+
 export const HeaderButton = styled(Button)`
   margin-top: 38px;
   text-align: center;
   height: 83px;
   width: 365px;
   background: linear-gradient(94deg, #a2da00 26.2%, #93c600 99.21%);
+  border: 1px solid #a2da00;
+  .ant-btn-primary:hover {
+    background: linear-gradient(94deg, #a2da00 26.2%, #93c600 99.21%);
+    border: 1px solid #a2da00;
+  }
   > span {
     color: #23262f;
     font-size: 30.376px;
     line-height: 2.3;
   }
+`;
+
+export const NavBarContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
 `;
--- src/app/common/Header/Header.tsx
+++ src/app/common/Header/Header.tsx
@@ -1,48 +1,115 @@
 "use client";
-
+import { usePathname } from "next/navigation";
 import Image from "next/image";
+import React, { useEffect, useState } from "react";
 import { Navbar } from "../Navbar/Navbar";
 import home from "../../../../public/images/home.svg";
 import logo from "../../../../public/images/logo.png";
-import { Button } from "antd";
 
 import * as Styled from "./Header.styled";
 
 export const Header: React.FC = () => {
+  const pathName = usePathname();
+  const [path, setPath] = useState<string>("/");
+
+  useEffect(() => {
+    setPath(pathName);
+  }, [path, pathName]);
+
+  const isHomePage = path === "/";
+
+  const headerTextContent = (
+    <>
+      {path === "/" && (
+        <>
+          <span style={{ color: "#C1FF0B" }}>ISLAMIC </span>DREAMS
+          INTERPRETATION{" "}
+        </>
+      )}
+      {path === "/dictionary" && <>Choose the first letter of the</>}
+    </>
+  );
+
+  const headerSubTextContent = (
+    <>
+      {path === "/" && (
+        <>
+          BECAUSE YOUR <span style={{ color: "#C1FF0B" }}>DREAMS</span>
+          <br />
+          ARE <span style={{ color: "#C1FF0B" }}>MEANINGFUL!</span>{" "}
+        </>
+      )}
+      {path === "/dictionary" && (
+        <>
+          Main <span style={{ color: "#C1FF0B" }}>Symbol</span>
+          <br />
+          in your <span style={{ color: "#C1FF0B" }}>Dream</span>{" "}
+        </>
+      )}
+      {path === "/videos" && (
+        <>
+          Welcome to our
+          <br />
+          <span style={{ color: "#C1FF0B" }}>Videos</span>{" "}
+        </>
+      )}
+      {path === "/about" && (
+        <>
+          <span style={{ color: "#C1FF0B" }}>About</span> Us{" "}
+        </>
+      )}
+    </>
+  );
+
+  const headerDescriptionContent = (
+    <>
+      {path === "/about" && (
+        <>
+          Alhodhod.com is dedicated since 2001 to the islamic interpretation of
+          dreams to help Muslims understanding the meanings of their dreams.
+        </>
+      )}
+    </>
+  );
+
   return (
     <Styled.HeaderContainer>
-      <Image
-        src={home}
-        alt="Home page background"
-        quality={100}
-        fill
-        style={{
-          objectFit: "cover",
-          borderRadius: "11px",
-        }}
-      />
-      <Styled.NavbarContainer>
-        <Navbar />
-      </Styled.NavbarContainer>
       <Styled.LogoContainer>
         <Image src={logo} alt="Al-hodhod logo" quality={100} />
         <Styled.WebsiteAddress>Alhodhod.com</Styled.WebsiteAddress>
         <Styled.WebsiteSince>EST 2001</Styled.WebsiteSince>
       </Styled.LogoContainer>
-      <Styled.HeaderTextContainer>
-        <Styled.HeaderText>
-          <span style={{ color: "#C1FF0B" }}>ISLAMIC </span>DREAMS
-          INTERPRETATION
-        </Styled.HeaderText>
-        <Styled.HeaderSubText>
-          BECAUSE YOUR <span style={{ color: "#C1FF0B" }}>DREAMS</span>
-          <br />
-          ARE <span style={{ color: "#C1FF0B" }}>MEANINGFUL!</span>
-        </Styled.HeaderSubText>
-        <Styled.HeaderButton href="/dictionary">
-          Dictionary of Dreams
-        </Styled.HeaderButton>
-      </Styled.HeaderTextContainer>
+      <Styled.HeroWrapper $isHomePage={isHomePage}>
+        <Styled.ImageWrapper>
+          <Image
+            priority
+            src={home}
+            layout="fill"
+            objectFit="cover"
+            objectPosition="center"
+            alt="home page background"
+            style={{ borderRadius: "11px" }}
+          />
+        </Styled.ImageWrapper>
+
+        <Styled.HeroContent>
+          <Styled.NavBarContainer>
+            <Navbar />
+          </Styled.NavBarContainer>
+          <Styled.HeaderTextContainer $isHomePage={isHomePage}>
+            <Styled.HeaderText>{headerTextContent}</Styled.HeaderText>
+            <Styled.HeaderSubText>{headerSubTextContent}</Styled.HeaderSubText>
+            <Styled.HeaderDescription>
+              {headerDescriptionContent}
+            </Styled.HeaderDescription>
+            {isHomePage && (
+              <Styled.HeaderButton href="/dictionary" type="primary">
+                Dictionary of Dreams
+              </Styled.HeaderButton>
+            )}
+          </Styled.HeaderTextContainer>
+        </Styled.HeroContent>
+      </Styled.HeroWrapper>
     </Styled.HeaderContainer>
   );
 };
