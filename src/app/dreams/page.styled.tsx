198a91b0a0ea77fea2cde0828a42a26b67c1ccf8
Hamzah S <hamzah.saeed@arkhitech.com>
2024-01-26 00:14:10 +0500

    dreams page now mobile responsive

--- src/app/dreams/page.styled.tsx
+++ src/app/dreams/page.styled.tsx
@@ -1,38 +1,66 @@
 import { styled } from "styled-components";
+import { breakpoint } from "../common/ui";
+import Image from "next/image";
 
 export const ListOfDreamsForSymbol = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
   padding: 0 4.58vw;
-  margin-top: 130px;
-  margin-bottom: 130px;
+  margin-top: 22px;
+  ${breakpoint.md} {
+    margin-top: 130px;
+    margin-bottom: 130px;
+  }
 `;
 
 export const SectionHeader = styled.div`
   font-style: normal;
   font-weight: 600;
-  font-size: 44.47px;
-  line-height: 54px;
-  margin-bottom: 83px;
+  font-size: 11px;
+  line-height: 13.41px;
+  margin-bottom: 37px;
+  text-align: center;
   color: #23262f;
+  ${breakpoint.md} {
+    font-size: 44.47px;
+    line-height: 54px;
+    margin-bottom: 83px;
+  }
+`;
+
+export const ArabesqueIcon = styled(Image)`
+  width: 184px;
+  height: 19px;
+  ${breakpoint.md} {
+    width: 713px;
+    height: 77px;
+  }
 `;
 
 export const DreamsListDiv = styled.div`
-  width: 1350px;
-  background-color: white;
+  width: 320px;
   padding-bottom: 32px;
   border-radius: 13px;
+  ${breakpoint.md} {
+    width: 1350px;
+    background-color: white;
+  }
 `;
 
 export const RightSideText = styled.div`
-  font-size: 28px;
-  font-weight: 400;
-  line-height: 35px;
+  font-size: 7px;
+  font-weight: 500;
+  line-height: 9px;
   letter-spacing: 0em;
   text-align: right;
-  margin: 8.25vh 3.25vw 8.25vh 0;
-  color: #5E5E5E;
+  color: #5e5e5e;
+  ${breakpoint.md} {
+    font-size: 28px;
+    font-weight: 400;
+    line-height: 35px;
+    margin: 8.25vh 3.25vw 8.25vh 0;
+  }
 `;
 
 export const DreamList = styled.div`
@@ -46,126 +74,185 @@ export const DreamItem = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin: 18px auto 18px auto;
-  width: 1200px;
+  margin: 11px auto 11px auto;
+  padding: 9px 0px;
+  width: 84.37vw;
   transition: border 0.1s;
-  border: 0.9px solid #F9F9F9;
-  border-radius: 10px;
-  background: #F9F9F9;
-  
-  &:hover {
-    box-shadow: 0 0 0 1.8px #C2FF0B, inset 0 0 0 1.8px #C2FF0B;
+  border: 0.9px solid white;
+  border-radius: 5px;
+  background: white;
+
+  ${breakpoint.md} {
+    width: 1200px;
+    border-radius: 10px;
+    background: #f9f9f9;
+    border: 0.9px solid #f9f9f9;
+    margin: 18px auto 18px auto;
+    &:hover {
+      box-shadow: 0 0 0 1.8px #c2ff0b, inset 0 0 0 1.8px #c2ff0b;
+    }
   }
+`;
+
+export const DreamItemCircleTextContainer = styled.div`
+  display: flex;
+  justify-content: left;
+  align-items: center;
 `;
 
 export const DreamItemCircle = styled.div`
   box-sizing: border-box;
-  width: 57.6px;
-  height: 57.6px;
+  width: 26.44px;
+  height: 26.44px;
   border-radius: 50%;
   display: flex;
   justify-content: center;
   align-items: center;
-  margin: 0 5.5vh;
-  font-size: 29.07px;
+  margin: 11px 10px;
+  font-size: 9.5px;
+  line-height: 12px;
   font-weight: 600;
-  line-height: 37.95px;
   letter-spacing: 0em;
   text-align: center;
   color: white;
   border: 0.891274px solid #979797;
   cursor: pointer;
-  background-color: #23262F;
-  margin: 25px 50px;
+  background-color: #23262f;
+  ${breakpoint.md} {
+    width: 57.6px;
+    height: 57.6px;
+    font-size: 29.07px;
+    line-height: 37.95px;
+    margin: 25px 50px;
+  }
 `;
 
 export const DreamItemText = styled.div`
-  font-size: 29px;
+  font-size: 11px;
   font-weight: 600;
-  line-height: 38px;
+  line-height: 15px;
   letter-spacing: 0em;
-  text-align: center;
-  color: #23262F;
+  text-align: left;
+  color: #23262f;
+  ${breakpoint.md} {
+    font-size: 29px;
+    line-height: 38px;
+  }
 `;
 
 export const MeaningText = styled.div`
-  font-size: 28px;
-  font-weight: 400;
-  line-height: 35px;
-  letter-spacing: 0em;
-  text-align: right;
-  color: #5E5E5E;
+  display: none;
+  ${breakpoint.md} {
+    font-size: 28px;
+    font-weight: 400;
+    line-height: 35px;
+    letter-spacing: 0em;
+    text-align: right;
+    color: #5e5e5e;
+  }
 `;
 
 export const DropDownCircle = styled.div`
   box-sizing: border-box;
-  width: 57.6px;
-  height: 57.6px;
+  width: 23.34px;
+  height: 23.34px;
   border-radius: 50%;
   display: flex;
   justify-content: center;
   align-items: center;
   margin: 0 2.5vw 0 0.5vw;
   text-align: center;
-  border: 0.891274px solid #616161;
+  &:hover {
+    background: linear-gradient(93.5deg, #a2da00 26.2%, #93c600 99.21%);
+  }
   cursor: pointer;
-  background-color: #F9F9F9;
+  background-color: #f9f9f9;
+  ${breakpoint.md} {
+    width: 57.6px;
+    height: 57.6px;
+    border: 0.891274px solid #616161;
+    &:hover {
+      background: none;
+    }
+  }
 `;
 
 export const DreamInterpretationDiv = styled.div`
   margin: 0 auto;
   margin-top: 3vh;
-  background: #F9F9F9;
-  width: 1200px;
+  background: white;
+  width: 84.37vw;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  border-radius: 10px;
-  box-shadow: 0 0 0 1.8px #C2FF0B, inset 0 0 0 1.8px #C2FF0B;
+  border-radius: 5px;
+  ${breakpoint.md} {
+    background: #f9f9f9;
+    box-shadow: 0 0 0 1.8px #c2ff0b, inset 0 0 0 1.8px #c2ff0b;
+    width: 1200px;
+    border-radius: 10px;
+  }
 `;
 
 export const DreamInterpretationHeader = styled.div`
-  margin-top: 6vh;
-  font-size: 32px;
+  margin: 1.5vh 0;
+  font-size: 11px;
   font-weight: 600;
-  line-height: 39px;
+  line-height: 15px;
   letter-spacing: 0em;
   text-align: center;
+  ${breakpoint.md} {
+    font-size: 32px;
+    line-height: 39px;
+    margin: 6vh 0;
+  }
 `;
 
 export const DreamInterpretationLine = styled.div`
   width: 68vw;
   height: 0px;
-  margin: 23px auto;
-  border: 0.891274px solid #e6e6e6;
+  border: 0.18px solid #e6e6e6;
+  ${breakpoint.md} {
+    margin: 23px auto;
+    border: 0.891274px solid #e6e6e6;
+  }
 `;
 
 export const DreamInterpretationSubText = styled.div`
   margin: 5vh 35px 0 35px;
-  font-size: 29px;
+  font-size: 11px;
   font-weight: 400;
-  line-height: 38px;
+  font-style: italic;
+  line-height: 14px;
   letter-spacing: 0em;
   text-align: center;
+  ${breakpoint.md} {
+    font-size: 29px;
+    font-style: normal;
+    line-height: 38px;
+  }
 `;
 
 export const BackToLettersButton = styled.a`
-  margin: 0 auto;
-  margin-top: 60px;
-  width: 276px;
-  height: 83px;
-  border: 1px solid;
-  border-color: #878787;
-  border-radius: 13px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-size: 30px;
-  font-weight: 500;
-  line-height: 37px;
-  letter-spacing: 0em;
-  text-align: center;
-  color: inherit;
-  text-decoration: none;
+  display: none;
+  ${breakpoint.md} {
+    margin: 0 auto;
+    margin-top: 60px;
+    width: 276px;
+    height: 83px;
+    border: 1px solid;
+    border-color: #878787;
+    border-radius: 13px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 30px;
+    font-weight: 500;
+    line-height: 37px;
+    letter-spacing: 0em;
+    text-align: center;
+    color: inherit;
+    text-decoration: none;
+  }
 `;
