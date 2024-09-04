6e356fb60c829666ee998e4169673ed097f15797
Shanza Gul <gulshanza77@gmail.com>
2024-09-04 18:40:57 +0500

    remove all icons except youtube

--- src/app/common/components/Footer/Footer.tsx
+++ src/app/common/components/Footer/Footer.tsx
@@ -18,75 +18,61 @@ import {
 export const Footer: React.FC = () => {
   const { t } = useTranslation();
   return (
-    <>
-      <Styled.FooterWrapper>
-        <Styled.FooterContainer>
-          <Row justify="space-between">
-            <Col>
-              <Styled.UiList>
-                <Styled.UiLink href="/">{t("Home")}</Styled.UiLink>
-                <Styled.UiLink href="/dictionary">{t("Dictionary of Dreams")}</Styled.UiLink>
-                <Styled.UiLink href="/blog">{t("Blog")}</Styled.UiLink>
-                <Styled.UiLink href="/videos">{t("Videos")}</Styled.UiLink>
-                <Styled.UiLink href="/about">{t("About Us")}</Styled.UiLink>
-              </Styled.UiList>
-            </Col>
-            <Col>
-              <Styled.LogoContainer>
-                <Styled.DesignImageDesktop
-                  src={logo}
-                  alt="Al-hodhod logo"
-                  quality={100}
-                />
-                <Styled.DesignImageMobile
-                  src={"/images/footerDesign.svg"}
-                  alt="Al-hodhod logo"
-                  width={90}
-                  height={130}
-                  quality={100}
-                />
-                <Styled.WebsiteAddress>Alhodhod.com</Styled.WebsiteAddress>
-                <Styled.WebsiteSince>EST 2001</Styled.WebsiteSince>
-              </Styled.LogoContainer>
-            </Col>
-          </Row>
-          <Styled.Divider style={{ backgroundColor: "#545D79" }} />
-          <Row justify="space-between">
-            <Col>
-              <Space size={4}>
-                <Styled.CRIcon>©</Styled.CRIcon>
-                <Styled.CopyRight>
-                  {t("Copyrights")}
-                </Styled.CopyRight>
-              </Space>
-            </Col>
-            <Col>
-              <Space>
-                <Styled.IconContainer>
-                  <FacebookFilled />
-                </Styled.IconContainer>
-                <Styled.IconContainer>
-                  <TwitterCircleFilled />
-                </Styled.IconContainer>
-                <Styled.IconContainer>
-                  <a
-                    href="https://www.youtube.com/@user-rl9rs7hi4y"
-                    target="_blank"
-                  >
-                    <YoutubeOutlined />
-                  </a>
-                </Styled.IconContainer>
-                <Styled.IconContainer>
-                  <LinkedinFilled />
-                </Styled.IconContainer>
-                <Styled.IconContainer>
-                  <InstagramFilled />
-                </Styled.IconContainer>
-              </Space>
-            </Col>
-          </Row>
-        </Styled.FooterContainer>
-      </Styled.FooterWrapper>
-    </>
+    <Styled.FooterWrapper>
+      <Styled.FooterContainer>
+        <Row justify="space-between">
+          <Col>
+            <Styled.UiList>
+              <Styled.UiLink href="/">{t("Home")}</Styled.UiLink>
+              <Styled.UiLink href="/dictionary">
+                {t("Dictionary of Dreams")}
+              </Styled.UiLink>
+              <Styled.UiLink href="/blog">{t("Blog")}</Styled.UiLink>
+              <Styled.UiLink href="/videos">{t("Videos")}</Styled.UiLink>
+              <Styled.UiLink href="/about">{t("About Us")}</Styled.UiLink>
+            </Styled.UiList>
+          </Col>
+          <Col>
+            <Styled.LogoContainer>
+              <Styled.DesignImageDesktop
+                src={logo}
+                alt="Al-hodhod logo"
+                quality={100}
+              />
+              <Styled.DesignImageMobile
+                src={"/images/footerDesign.svg"}
+                alt="Al-hodhod logo"
+                width={90}
+                height={130}
+                quality={100}
+              />
+              <Styled.WebsiteAddress>Alhodhod.com</Styled.WebsiteAddress>
+              <Styled.WebsiteSince>EST 2001</Styled.WebsiteSince>
+            </Styled.LogoContainer>
+          </Col>
+        </Row>
+        <Styled.Divider style={{ backgroundColor: "#545D79" }} />
+        <Row justify="space-between">
+          <Col>
+            <Space size={4}>
+              <Styled.CRIcon>©</Styled.CRIcon>
+              <Styled.CopyRight>{t("Copyrights")}</Styled.CopyRight>
+            </Space>
+          </Col>
+          <Col>
+            <Space>
+              <Styled.IconContainer>
+                <a
+                  href="https://www.youtube.com/@user-rl9rs7hi4y"
+                  target="_blank"
+                >
+                  <YoutubeOutlined />
+                </a>
+              </Styled.IconContainer>
+            </Space>
+          </Col>
+        </Row>
+      </Styled.FooterContainer>
+    </Styled.FooterWrapper>
   );
 };
